ca2fde873d38934a8ac718a1339b7f028aa5764d
Bartosz <bartosz.burdalski@gmail.com>
2019-04-12 17:02:59 +0200

    Added an animation to the header

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,6 +3,16 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div className="page">
+            <div className="page__wrapper">
+                <h1 className="page__title">Witamy na stronie Sweet Muffin!</h1>
+                <section className="page__section">
+                    <p>Zapraszamy do przejrzenia naszej oferty. Proszę zajrzeć też do zakładki "O nas":)</p>
+                </section>
+            </div>
+
+            {/* <div className="page__slider">
+                HEJAK
+            </div> */}
         </div>
     );
 }
