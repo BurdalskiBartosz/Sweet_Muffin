67ffa90db7fd8b7c68ae81c8de8ad5a8f0f48087
Bartosz <bartosz.burdalski@gmail.com>
2019-04-14 20:26:32 +0200

    Added simple condition with sessionStorage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 const AboutPage = () => {
     return (
-        <div>HEJ</div>
+        <div className='wrapper'>
+            <h2>Sweet Muffin</h2>
+            <p className='wrapper__text'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore verit inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboci velit, sed quia non numquum exercitationem ullam corporis suscipit unde omnis iste natus error sit volu ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        </div>
     );
 }
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from '../components/Slider/Slider';
 
-const MainPage = () => {
-    return (
-        <div className="page">
-            <div className="page__wrapper">
-                <h1 className="page__title">Witamy na stronie Sweet Muffin!</h1>
-                <p>Zapraszamy do przejrzenia naszej oferty. Proszę zajrzeć też do zakładki "O nas":)</p>
+// const MainPage = () => {
+
+//     return (
+//         <div className="page">
+//             <div className="page__wrapper">
+//                 <h1 className="page__title">Witamy na stronie Sweet Muffin!</h1>
+//                 <p>Zapraszamy do przejrzenia naszej oferty. Proszę zajrzeć też do zakładki "O nas":)</p>
+//             </div>
+//             <Slider />
+//         </div>
+//     );
+// }
+
+// export default MainPage;
+
+class MainPage extends Component {
+
+    componentDidMount() {
+        if (sessionStorage.length === 0) {
+            sessionStorage.setItem("class", false);
+            return document.querySelector('.page').classList.add('page-animation')
+        }
+        else {
+            return document.querySelector(".page").classList.add('page-withoutAnimation')
+        }
+    }
+
+    render() {
+        return (
+            <div className="page">
+                <div className="page__wrapper">
+                    <h1 className="page__title">Witamy na stronie Sweet Muffin!</h1>
+                    <p>Zapraszamy do przejrzenia naszej oferty. Proszę zajrzeć też do zakładki "O nas":)</p>
+                </div>
+                <Slider />
             </div>
-            <Slider />
-        </div>
-    );
+        );
+    }
 }
 
 export default MainPage;
