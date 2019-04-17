13c7827527c75784270d37a9d846bdae215bfa58
Bartosz <bartosz.burdalski@gmail.com>
2019-04-17 23:13:43 +0200

    Simple view of ServicesPage component

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const ServicesPage = () => {
     return (
-        <div>SERVICESPAGE</div>
+        <div className="servicesPage">
+            <h3>Nasza cukiernia poleca</h3>
+            <div className="servicesPage__wrapper">
+                <section className="servicesPage__section">
+                    <h4>Muffinowe Torty Ślubne</h4>
+                    <p>Lorem ispum Lrem ispum Lem ispu Lem ispm Lorem ispum </p>
+                </section>
+                <section className="servicesPage__section">
+                    <h4>Party Muffinowe</h4>
+                    <p>Lorem ispum Lrem ispum Lem ispu Lem ispm Lorem ispum </p>
+                </section>
+            </div>
+        </div>
     );
 }
 
