3bc342621597e701d38431616fdcbab579481f40
Bartosz <bartosz.burdalski@gmail.com>
2019-05-15 20:02:15 +0200

    AboutPage ended, ready to write responsive

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer">DZIALA FOOTER</div>
+        <div className="footer">
+            <span>Made by Bartosz Burdalski &copy; 2019</span>
+            <a href="https://www.facebook.com/bartek.burdalski"><i className="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/bartoszburdalski/"><i className="fab fa-instagram"></i></a>
+        </div>
     );
 }
 
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import about1 from '../images/About images/ImgAbout1.jpg'
+import about2 from '../images/About images/ImgAbout2.jpg'
+import about3 from '../images/About images/ImgAbout3.jpg'
 const AboutPage = () => {
     return (
         <div className='wrapper'>
-            <h2>Sweet Muffin</h2>
-            <p className='wrapper__text'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore verit inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboci velit, sed quia non numquum exercitationem ullam corporis suscipit unde omnis iste natus error sit volu ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+            <h2 className="about__title">Sweet Muffin</h2>
+            <p className='wrapper__text'><img className="about__img" alt="An exhibition of cookies, many different types" src={about1} />Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam,<img alt="" className="about__img about__img-right__img" src={about3} /> eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictaperiam, eaque ipsa quae ab illo inventore verit inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboci velit, sed<img alt="" className="about__img about__img-right__img" src={about2} />quia non numquum exercitationem ullam corporis suscipit unde omnis iste natus error sit volu ex ea commodi consequatur?</p>
         </div>
     );
 }
