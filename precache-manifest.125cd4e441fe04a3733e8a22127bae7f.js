374bd9a59d9798869d8d86ed86fe6fa4ea6c2016
Bartosz <bartosz.burdalski@gmail.com>
2019-05-21 22:19:28 +0200

    Updates

---
+++ precache-manifest.125cd4e441fe04a3733e8a22127bae7f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c65fd47d030346aa768",
+    "url": "/Sweet_Muffin-react/static/css/main.0c4184c9.chunk.css"
+  },
+  {
+    "revision": "9c65fd47d030346aa768",
+    "url": "/Sweet_Muffin-react/static/js/main.926e6d66.chunk.js"
+  },
+  {
+    "revision": "e1a1f9385c42e1c22de0",
+    "url": "/Sweet_Muffin-react/static/js/runtime~main.a55e9d69.js"
+  },
+  {
+    "revision": "2b9f653802bca6a35312",
+    "url": "/Sweet_Muffin-react/static/js/2.949cf8e3.chunk.js"
+  },
+  {
+    "revision": "ee05216c2a965b18d593e88cc3d79b3b",
+    "url": "/Sweet_Muffin-react/static/media/serv_img1.ee05216c.jpg"
+  },
+  {
+    "revision": "b6db3ad154401875d7284e69cd463b44",
+    "url": "/Sweet_Muffin-react/static/media/img1.b6db3ad1.jpg"
+  },
+  {
+    "revision": "fa7025c017cc4b6b4933f8042668d596",
+    "url": "/Sweet_Muffin-react/static/media/img2.fa7025c0.jpg"
+  },
+  {
+    "revision": "37c9652c8c34fe68c68aa4a5361ab77a",
+    "url": "/Sweet_Muffin-react/static/media/img3.37c9652c.jpg"
+  },
+  {
+    "revision": "b7cc467b3f0dc163afe39a52068e6094",
+    "url": "/Sweet_Muffin-react/static/media/img4.b7cc467b.jpg"
+  },
+  {
+    "revision": "c1b393399a44d491110495a40ab81310",
+    "url": "/Sweet_Muffin-react/static/media/img5.c1b39339.jpg"
+  },
+  {
+    "revision": "9bfe2d94ae8ea311849daa6a6d794aef",
+    "url": "/Sweet_Muffin-react/static/media/cake1.9bfe2d94.jpg"
+  },
+  {
+    "revision": "69f082f54651295c449f541032ee417f",
+    "url": "/Sweet_Muffin-react/static/media/cake2.69f082f5.jpg"
+  },
+  {
+    "revision": "20ff2a2aa12be8fe37fc5a66a4f04fb9",
+    "url": "/Sweet_Muffin-react/static/media/cake3.20ff2a2a.jpg"
+  },
+  {
+    "revision": "f909a772a85df786e25f50466d29e30b",
+    "url": "/Sweet_Muffin-react/static/media/cake6.f909a772.jpg"
+  },
+  {
+    "revision": "d38d83290bfe6e5697c51247c5761372",
+    "url": "/Sweet_Muffin-react/static/media/cake7.d38d8329.jpg"
+  },
+  {
+    "revision": "edcc706eccbd97d1a1b1816c8f750ea4",
+    "url": "/Sweet_Muffin-react/static/media/cake9.edcc706e.jpg"
+  },
+  {
+    "revision": "6c5fdd5d726b6a61b1e9e9432f2c2230",
+    "url": "/Sweet_Muffin-react/static/media/cake10.6c5fdd5d.jpg"
+  },
+  {
+    "revision": "50437609b1406b5ba0bd3f16a5c24f0e",
+    "url": "/Sweet_Muffin-react/static/media/cake11.50437609.jpg"
+  },
+  {
+    "revision": "46225ff4c49fb30d0d20f877711704de",
+    "url": "/Sweet_Muffin-react/static/media/cake12.46225ff4.jpg"
+  },
+  {
+    "revision": "1b3b2a5e8bcbb7d2b31a5f656f2e7ef1",
+    "url": "/Sweet_Muffin-react/static/media/serv_img2.1b3b2a5e.jpg"
+  },
+  {
+    "revision": "d11a2cdeeabcdda28b1a69f99fb435b5",
+    "url": "/Sweet_Muffin-react/static/media/ImgAbout1.d11a2cde.jpg"
+  },
+  {
+    "revision": "61c63a37fdaeeb1385699773ebe67d68",
+    "url": "/Sweet_Muffin-react/static/media/ImgAbout2.61c63a37.jpg"
+  },
+  {
+    "revision": "6fe428c3e561b1f42607fe854b1967ed",
+    "url": "/Sweet_Muffin-react/static/media/ImgAbout3.6fe428c3.jpg"
+  },
+  {
+    "revision": "d3a7f27108e6f455f3cf5c433f28db3a",
+    "url": "/Sweet_Muffin-react/static/media/panels_small-min.d3a7f271.jpeg"
+  },
+  {
+    "revision": "c6fe170dbec9011a11453028f6fc08cf",
+    "url": "/Sweet_Muffin-react/index.html"
+  }
+];
